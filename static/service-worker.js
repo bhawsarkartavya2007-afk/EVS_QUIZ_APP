b4f1b849a1a0381314adb61406df2aa34e366be5
bhawsarkartavya2007-afk <bhawsarkartavya2007@gmail.com>
2026-06-14 00:06:56 +0530

    Update service-worker.js

---
+++ static/service-worker.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', (event) => {
+  console.log('Service Worker installed');
+});
+
+self.addEventListener('fetch', (event) => {
+  // Ye file app ko chalne mein madad karti hai
+});
